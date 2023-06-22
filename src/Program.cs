2a4d495bda44f695e00d1275979c01cecee32ea7
Mateusz Plewka <bartosztalarek23@protonmail.com>
2023-06-22 17:42:16 +0200

    Revert "main - removed console.writeline from program.cs"
    This reverts commit 076878aa40e48cb916492da1a189d4ff631d0ac5.

--- src/Program.cs
+++ src/Program.cs
@@ -6,6 +6,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Math app!");
         using IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
